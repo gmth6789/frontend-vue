86905c93d8cebb88b26f07017e69b928eb1ebff3
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-09-02 12:07:50 +0700

    3 files modified, 2 added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:'./', // เพิ่มคุณลักษณะนี้
+  build: {
+  },
+  define: {
+    'process.env': {},
+  },
+  resolve: {
+    // กำหนดค่านามแฝงเส้นทาง
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
